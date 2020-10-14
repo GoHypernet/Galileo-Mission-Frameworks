547f4a421b7487bd6b300839fe143dc7de6786c1
TtheBC01 <todd@hyperdyne.io>
2020-10-14 15:50:27 -0700

    update

--- runxmrig.sh
+++ runxmrig.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-xmrig --donate-level $DONATE_LVL --cuda --cuda-loader=$LIBXMRIG_CUDA -o $POOL:$PORT -u $WALLET -k --tls --rig-id $RIG_ID
+#xmrig --donate-level $DONATE_LVL --cuda --cuda-loader=$LIBXMRIG_CUDA -o $POOL:$PORT -u $WALLET -k --tls --rig-id $RIG_ID
+xmrig --donate-level $DONATE_LVL -o $POOL:$PORT -u $WALLET -k --tls --rig-id $RIG_ID  --randomx-1gb-pages -t $(nproc)
