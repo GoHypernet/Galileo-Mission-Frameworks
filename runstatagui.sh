3a2c0e6a17f0e9277bccf33cc16ddf8f1e629f44
unknown <todd@hypernetlabs.io>
2021-07-09 16:21:28 -0700

    update to Stata

--- runstatagui.sh
+++ runstatagui.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-if test -e ./stata.lic; then
-   echo "License file detected, starting Stata"
-   mv ./stata.lic /usr/local/stata16/stata.lic
-   sleep 1
-   $STATA
-elif test -e ./STATA.LIC; then
-   echo "License file detected, starting Stata"
-   mv ./STATA.LIC /usr/local/stata16/stata.lic
-   sleep 1
-   $STATA 
+if test -e /home/galileo/stata.lic; then
+   echo "License file detected, placing in Stata path."
+   mv /home/galileo/stata.lic /usr/local/stata16/stata.lic
+elif test -e /home/galileo/STATA.LIC; then
+   echo "License file detected, placing in Stata path."
+   mv /home/galileo/STATA.LIC /usr/local/stata16/stata.lic
+elif test -e /theia/stata.lic; then
+   echo "License file detected, placing in Stata path."
+   mv /theia/stata.lic /usr/local/stata16/stata.lic
+elif test -e /theia/STATA.LIC; then
+   echo "License file detected, placing in Stata path."
+   mv /theia/STATA.LIC /usr/local/stata16/stata.lic
 else
    echo "--------------------------------------------------------------------------"
    echo "Please provide a valid stata.lic file for Stata16"
-   echo "See https://hypernetlabs.io/galileo/tutorials/tutorial-stata/ for more details"
    echo "--------------------------------------------------------------------------"
 fi
